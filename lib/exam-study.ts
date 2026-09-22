fc512af9e1f49a30de2ae0facad1020e401870f0
Weather-Mister <weatherisgood06@gmail.com>
2026-09-22 23:51:37 +0800

    Add Exam Study Week 1 vocabulary

--- lib/exam-study.ts
+++ lib/exam-study.ts
@@ -20,6 +20,31 @@ export type ExamStudySet={
  * device-local weekly Mastered checklist so exam lists remain isolated.
  */
 export const examStudySets:ExamStudySet[]=[
+ {
+  id:1,
+  title:'Week 1',
+  words:[
+   {traditional:'您',pinyin:'nín',meaning:'you (formal and respectful)'},
+   {traditional:'貴姓',pinyin:'guìxìng',meaning:'May I know your last name?'},
+   {traditional:'姓',pinyin:'xìng',meaning:'surname; family name; last name'},
+   {traditional:'李',pinyin:'Lǐ',meaning:'a common Chinese surname'},
+   {traditional:'先生',pinyin:'xiānshēng / xiānsheng',meaning:'Mr.; Sir; gentleman; husband'},
+   {traditional:'王',pinyin:'Wáng',meaning:'a common Chinese surname'},
+   {traditional:'我',pinyin:'wǒ',meaning:'I; me'},
+   {traditional:'叫',pinyin:'jiào',meaning:'to be called (by the name of); to call'},
+   {traditional:'好',pinyin:'hǎo',meaning:'to be good; well'},
+   {traditional:'是',pinyin:'shì',meaning:'to be (am, are, is)'},
+   {traditional:'美國人',pinyin:'Měiguórén',meaning:'American'},
+   {traditional:'美國',pinyin:'Měiguó',meaning:'U.S.A.; America'},
+   {traditional:'國',pinyin:'guó',meaning:'country; nation'},
+   {traditional:'人',pinyin:'rén',meaning:'person'},
+   {traditional:'嗎',pinyin:'ma',meaning:'question particle'},
+   {traditional:'不',pinyin:'bù / bú',meaning:'not'},
+   {traditional:'英國',pinyin:'Yīngguó',meaning:'England; Britain'},
+   {traditional:'你',pinyin:'nǐ',meaning:'you'},
+   {traditional:'什麼',pinyin:'shénme',meaning:'what'},
+  ],
+ },
 ];
 
 const hanziPattern=/[\u3400-\u9fff\uf900-\ufaff]/;
